c0f93556c95ff5698d1d0290e37d3b4e8557b518
Boris Damevin <borisdamevin@gmail.com>
2012-10-10 21:13:58 +0200

    Plugin test

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,7 +2,7 @@
  * V1.0
  * Author : Boris Damevin
  * Licence CC-BY http://creativecommons.org/licenses/by/3.0/fr/
- */
+
 
 $(document).ready(function(){
 
@@ -18,4 +18,28 @@ $(document).ready(function(){
         "left": ((windowWidth / 2) - (elementWidth / 2))
     });
 
-});
+});*/
+
+// project:
+// author:
+// date:
+//
+// description:
+// jquery version:
+//
+jQuery(function(){
+    function($){
+        $.fn.my_plugin = function(args){
+            args = $.extend({
+                key_1: "default_1",
+                key_2: "default_2"
+            }, args);
+            // place your code here
+
+
+            // eoc
+            return this;
+        };
+
+    }(jQuery);
+}); 
