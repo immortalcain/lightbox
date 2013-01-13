177ecd0a1754896d07b0df17a991f2b4b3d73863
Boris Damevin <borisdamevin@gmail.com>
2013-01-13 22:09:45 +0100

    Mise à jour description

--- js/lightbox.js
+++ js/lightbox.js
@@ -8,6 +8,7 @@
  *
  * Description: LightBox
  * jQuery: 1.8+
+ * jQuery UI: 1.9+
  * Plugin(s):
  *  - jquery-cookie.js (https://github.com/carhartl/jquery-cookie)
  * Browsers: IE7+, Firefox 5+, Chrome 20+, Safari 3+, Opera 10+
